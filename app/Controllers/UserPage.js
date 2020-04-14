4f48baa1ef1072acabae50817673eff79691bb82
ozzmo <mestervalera@gmail.com>
2020-04-14 23:15:01 +0500

    convert wall component into universal component for posts and messages

--- app/Controllers/UserPage.js
+++ app/Controllers/UserPage.js
@@ -5,11 +5,11 @@ define('app/Controllers/UserPage.js', [
     "app/Components/ProfilePhotos.js",
     "app/Components/ProfileAvatar.js",
     "app/Components/ProfileActions.js",
-    "app/Components/Wall.js",
+    "app/Components/Message.js",
     'css!assets/libs/normalize/normalize.css',
     'css!assets/libs/fa/scss/fontawesome.css',
     'css!assets/css/theme.css',
-], function (Controller, Header, ProfileInfo, ProfilePhotos, ProfileAvatar, ProfileActions, Wall) {
+], function (Controller, Header, ProfileInfo, ProfilePhotos, ProfileAvatar, ProfileActions, Message) {
 
     return class UserPage extends Controller {
 
@@ -41,6 +41,25 @@ define('app/Controllers/UserPage.js', [
                 text:"Остановился ли волчок... Кто знает...",
             }]
 
+            this.messagesData = [{
+                avatar:"assets/img/6_square.jpg",
+                name:"Дэвид Грей",
+                date:"Вчера 16:10",
+                text:"Давно тебя не было в уличных гонках!",
+            },
+            {
+                avatar:"assets/img/7_square.jpg",
+                name:"Мак ДеМарко",
+                date:"Вчера 16:10",
+                text:"Ты уже послушала мой новый альбом? Тебе понравится, обещаю.",
+            },
+            {
+                avatar:"assets/img/8_square.jpg",
+                name:"Девочка в пуховике",
+                date:"Вчера 16:10",
+                text:"Я к вам пишу — чего же боле? Что я могу ещё сказать? Теперь, я знаю, в вашей воле Меня презреньем наказать.",
+            }]
+
         }
 
         /**
@@ -56,11 +75,12 @@ define('app/Controllers/UserPage.js', [
                         <aside>
                             ${new ProfileInfo()}
                             ${new ProfilePhotos()}
-                            ${new Wall(this.postsData)}
+                            ${new Message(this.postsData, 'post')}
                         </aside>
                         <div>
                             ${new ProfileAvatar()}
                             ${new ProfileActions()}
+                            ${new Message(this.messagesData, 'message')}
                         </div>
                     </section>
                 </div>
