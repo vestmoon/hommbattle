d8851c1ad4ea785bc19d3aa38ca409285d201b0f
ozzmo <mestervalera@gmail.com>
2020-04-14 15:57:08 +0500

    wall component fixes

--- app/Components/Wall.js
+++ app/Components/Wall.js
@@ -1,7 +1,7 @@
 define(
   'app/Components/Wall.js', [
     'app/Components/Component.js',
-    'css!assets/css/Wall.css'
+    'css!app/Components/Wall.css'
   ],
   function (Component) {
 
@@ -10,11 +10,11 @@ define(
       /**
        * Инициализация компонента
        */
-      constructor() {
+      constructor(posts) {
 
         // Функция, вызывающая родительский конструктор
         super();
-
+        this.posts = posts;
       }
 
       /**
@@ -22,21 +22,41 @@ define(
        * @returns {string}
        */
       render() {
-
+        this.renderPosts()
         // Возвращение рендера
         return `
               <div class="block wall">
-                <div class="wall-head">
-                  <img class="block__img wall-head__img" src="assets/img/people-square.jpg" alt=""/>
-                  <span class="wall-head__name" title="Эммелин Лоуренс">Эммелин Лоуренс</span>
-                  <span class="wall-head__date" title="Вчера 14:33">Вчера 14:33</span>
-                </div>
-                <div class="wall-body">
-                  <p class="wall-body__text" title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, fuga!">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, fuga!</p>
-                  <img class="block__img wall-body__img" src="assets/img/2_full.jpg" alt=""/>
-                </div>
+                ${this.renderPosts()}
               </div>
               `;
+      }
+
+      renderPosts(){
+        let posts = '';
+        this.posts.forEach(post => {
+          posts += `
+              <div class="post">
+                <div class="post-head">
+                  <img class="block__img post-head__img" src="${post.avatar}" alt=""/>
+                  <span class="post-head__name" title="${post.name}">${post.name}</span>
+                  <span class="post-head__date" title="${post.date}">${post.date}</span>
+                </div>
+                <div class="post-body">
+                  <p class="post-body__text" title="${post.text}">${post.text}</p>
+                  ${this.renderPostImage(post.img)}
+                </div>
+              </div>
+          `
+        });
+        return posts;
+      }
+      
+      renderPostImage(img){
+        if (img){
+          return `<img class="block__img post-body__img" src="${img}" alt=""/>`;
+        } else {
+          return ``;
+        }
       }
 
     };
