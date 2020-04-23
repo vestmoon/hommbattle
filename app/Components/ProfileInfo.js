ce1168e487281775d4b444a9463574fb5f2c80b8
Arthur <52240661+ArthurKharisov@users.noreply.github.com>
2020-04-23 10:21:34 +0500

    Update ProfileInfo.js

--- app/Components/ProfileInfo.js
+++ app/Components/ProfileInfo.js
@@ -16,11 +16,13 @@
 define(
     'app/Components/ProfileInfo.js', [
         'app/Components/Component.js',
+        'app/Helpers/Declensions.js',
         'css!app/Components/ProfileInfo.css'
-    ], function (Component) {
+    ], function (Component, Declensions) {
         
 		const MILLISECONDS = (60 * 60 * 24 * 1000 * 365);
         const undefine = "Неизвестно";
+        const userKeyRegExp = /^[0-3][0-9]\.[0-1][1-9]\.[1-2][0-9]{3}$/;
         
         return class ProfileInfo extends Component {
             
@@ -30,23 +32,31 @@ define(
              */
             constructor() {
                 super();
+                this.data = {};
             }
             
 			/**
-             * Приводим дату рождения к виду "дата месяц, кол-во лет"
+             * Геттер для получения даты рождения в виде "дата месяц"
              */
-            birthday(data){
-                let yearString = ["год", "года", "лет"];
-                let months = [ "января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря" ];
-                let currentTime = new Date();
-                if(data.bDay != null) {
-                    let numbers = data.bDay.split('.');
+            get bMonth(){
+                if(userKeyRegExp.test(this.data.bDay)) {
+                    let months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+                    let numbers = this.data.bDay.split('.');
                     let bornTime = new Date(numbers[2], numbers[1], numbers[0]);
-                    let bornYear = Math.floor((currentTime-bornTime) / MILLISECONDS);
-                    let bornYearStr = yearString[(bornYear % 100 > 4 && bornYear % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(bornYear % 10 < 5) ? bornYear % 10 : 5]];
-                    return `${numbers[0]} ${months[bornTime.getMonth()-1]}, ${bornYear} ${bornYearStr}`;
-                } else {
-                    return undefine;
+                    return `${numbers[0]} ${months[bornTime.getMonth() - 1]}`;
+                }
+            }
+
+            /**
+             * Геттер для получения кол-во лет пользователя
+             */
+            get age() {
+                if(userKeyRegExp.test(this.data.bDay)) {
+                    let currentTime = new Date();
+                    let numbers = this.data.bDay.split('.');
+                    let bornTime = new Date(numbers[2], numbers[1], numbers[0]);
+                    let bornYear = Math.floor((currentTime - bornTime) / MILLISECONDS);
+                    return Declensions.getSupplementPrefix(bornYear, ["год", "года", "лет"]);
                 }
             }
             
@@ -54,7 +64,7 @@ define(
             moreButton() {
                 let block = document.querySelector(".user__fullinfo");
                 block.classList.toggle("visible");
-                this.innerText == "Подробнее" ? this.innerText="Скрыть подробности" : this.innerText="Подробнее";
+                this.innerText = (this.innerText == "Подробнее") ? "Скрыть подробности" : "Подробнее";
             }
 
             /**
@@ -73,17 +83,17 @@ define(
              * Метод обновления компонента после получения данных
              * @param data
              */
-            update(data) {
-                let mainInfo = this.renderInfo("День рождения", this.birthday(data) || undefine);
-                mainInfo += this.renderInfo("Город", data.city || undefine);
+            update() {
+                let mainInfo = this.renderInfo("День рождения", `${this.bMonth || undefine}, ${this.age || undefine}`);
+                mainInfo += this.renderInfo("Город", this.data.city || undefine);
 
-                let additionalInfo = this.renderInfo("Образование", `${data.edu || undefine} ${data.eduYear || undefine}`);
-                additionalInfo += this.renderInfo("Место работы", data.jobName || undefine);
+                let additionalInfo = this.renderInfo("Образование", `${this.data.edu || undefine} ${this.data.eduYear || undefine}`);
+                additionalInfo += this.renderInfo("Место работы", this.data.jobName || undefine);
 
                 let block = document.createElement('div');
                 block.innerHTML = `
-                        <h2 class="user__name">${data.firstName || undefine} ${data.lastName || undefine}</h2>
-                        <p class="user__status">${data.status || undefine}</p>
+                        <h2 class="user__name">${this.data.firstName || undefine} ${this.data.lastName || undefine}</h2>
+                        <p class="user__status">${this.data.status || undefine}</p>
                         ${mainInfo}
                         <p class="button button__user button__user_blue">Подробнее</p>
                         <div class="user__fullinfo">${additionalInfo}</div>
@@ -111,7 +121,8 @@ define(
                 let promise = this.getData("https://tensor-school.herokuapp.com/user/current", {credentials: 'include'})
                 promise.then(
                     result => {
-                        this.update(result.data);
+                        this.data = result.data;
+                        this.update();
                     },
                     error => {
                         document.getElementById(this.id).innerText = "Connection Error";
