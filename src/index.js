04af5b83a15bc2e87a74d38c82f277b11e828c90
Zotov A.V <vesst94@mail.ru>
2020-07-17 00:01:33 +0500

    перенес репозиторий

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import BattleField from './battle/main';
+
+ReactDOM.render(<BattleField />, document.getElementById('root'));
