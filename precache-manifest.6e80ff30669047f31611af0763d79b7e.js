e7fe9dafa841162f1c2684721ec68c683c89096e
Zotov A.V <vesst94@gmail.com>
2020-07-20 17:32:53 +0500

    Updates

---
+++ precache-manifest.6e80ff30669047f31611af0763d79b7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1389b5afea75fc6856d31acec121b5a3",
+    "url": "/hommbattle/index.html"
+  },
+  {
+    "revision": "80f2bca922eb79c0f7c6",
+    "url": "/hommbattle/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c1035075160c39716a6",
+    "url": "/hommbattle/static/js/2.66ebdc2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hommbattle/static/js/2.66ebdc2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f2bca922eb79c0f7c6",
+    "url": "/hommbattle/static/js/main.e33afacd.chunk.js"
+  },
+  {
+    "revision": "83f0ed71a6700b35b218",
+    "url": "/hommbattle/static/js/runtime-main.b70d9493.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hommbattle/static/media/logo.5d5d9eef.svg"
+  }
+]);
